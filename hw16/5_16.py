060ea8b750d66dcec2a1495b4a3bcd26830c5228
ELatestark <ravenjkeee@gmail.com>
2021-08-23 22:53:49 +0700

    edited hw 5.16

--- hw16/5_16.py
+++ hw16/5_16.py
@@ -1,14 +1,10 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # get location and google maps link from coordinates
 from geopy.geocoders import Nominatim
 coordsfile = passwd_file = open('/etc/file.txt', 'r')
 coordsfile_lines = coordsfile.readlines()
 coords = [x.replace("'", "").replace('"', "").split(',') for x in coordsfile_lines[0].split(';')]
-c1, c2=0, 0
-for i in range(len(coords[0])):
-    c1+=int(coords[0][i])/60**i
-for i in range(len(coords[1])):
-    c2 += int(coords[1][i]) / 60 ** i
-place = Nominatim(user_agent='hw16').reverse((c1, c2))
+coords = [[int(coords[i][j])/60**j for j in range(len(coords[i]))] for i in range(2)]
+place = Nominatim(user_agent='hw16').reverse((str(sum(coords[0])), str(sum(coords[1]))))
 print(place)
-print('https://www.google.com/maps/search/?api=1&query='+str(c1)+','+str(c2))
+print('https://www.google.com/maps/search/?api=1&query=' + str(sum(coords[0])) + ',' + str(sum(coords[1])))
