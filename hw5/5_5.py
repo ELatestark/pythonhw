0302b2fb413fbe118c4141370f617341c9b5132f
ELatestark <ravenjkeee@gmail.com>
2021-08-28 10:40:55 +0700

    edited hw 5.5

--- hw5/5_5.py
+++ hw5/5_5.py
@@ -1,8 +1,8 @@
 # !/usr/bin/env python3
 # print least missing number
-user_input=input().split()
+user_input=input('Insert any combinations of numbers, separated by Space:\n').split()
 list_from_input=results = list(map(int, user_input))
-for i in range(1, max(list_from_input)+2):
+for i in range(min(list_from_input), max(list_from_input)+2):
     if i not in list_from_input:
         break
 print(i)
