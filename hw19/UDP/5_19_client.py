b98286a654bfbac004b915401a403ab7b8112766
ELatestark <ravenjkeee@gmail.com>
2021-08-31 22:42:30 +0700

    added UDP version

---
+++ hw19/UDP/5_19_client.py
@@ -0,0 +1,14 @@
+import socket
+while 1:
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.connect(('localhost', 1337))
+        sock.send(input("Insert domain name to resolve it into IP:\nOR\nInsert command 'hostadd %hostname%:x.x.x.x' where x represent ipv4 format:\n").encode())
+
+        data = sock.recv(158)
+
+
+        print(data.decode())
+    except KeyboardInterrupt:
+        sock.close()
+        exit()
---
+++ hw19/UDP/5_19_server.py
@@ -0,0 +1,49 @@
+import socket
+import validators
+import dns.resolver
+
+
+def validate_ip(s):
+    a = s.split('.')
+    if len(a) != 4:
+        return False
+    for x in a:
+        if not x.isdigit():
+            return False
+        i = int(x)
+        if i < 0 or i > 255:
+            return False
+    return True
+
+while 1:
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind(('localhost', 1337))
+    conn, addr = sock.recvfrom(158)
+
+
+    try:
+        data = conn.decode()
+        if data.startswith("hostadd "):
+            out_file = open('hosts', 'a')
+            if validate_ip(data.split(':')[1]):
+                out_file.write(data.replace('hostadd ','') + '\n')
+                sock.sendto('Host added.'.encode(), addr)
+            else:
+                sock.sendto('Incorrect IP'.encode(), addr)
+        elif validators.url('http://'+data):
+            resolve_ip = dns.resolver.resolve(data, 'A')
+            for resolver_ip in resolve_ip:
+                sock.sendto(str(resolver_ip).encode(), addr)
+        else:
+                out_file = open('hosts', 'r')
+                hosts_lines = out_file.readlines()
+                for line in hosts_lines:
+                    if data == line.split(':')[0]:
+                        sock.sendto(line.split(':')[1].encode(), addr)
+                else:
+                    sock.sendto('No such record or site unavailable.'.encode(), addr)
+    except KeyboardInterrupt:
+        out_file.close()
+        conn.close()
+        exit()
