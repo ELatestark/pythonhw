e0c2dbdd5de646fb5ae64185545e79695cc1d508
ELatestark <ravenjkeee@gmail.com>
2021-09-05 20:50:47 +0700

    edited hw9_2

--- hw9/9_2.py
+++ hw9/9_2.py
@@ -1,5 +1,5 @@
 # !/usr/bin/env python3
 # projecteuler problem 9. There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
-print([(a*b*c) for a in range(1001) for b in range(a) for c in range(b) if a*a == b*b + c*c and a+b+c==1000])
+print(*[(a*b*c) for a in range(501) for b in range(a) for c in range(b) if a*a == b*b + c*c and a+b+c==1000])
 
