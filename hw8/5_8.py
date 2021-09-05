c61382b13fdf51e65785942c0039e4d84848058d
ELatestark <ravenjkeee@gmail.com>
2021-09-05 17:38:08 +0700

    edited hw5.8

--- hw8/5_8.py
+++ hw8/5_8.py
@@ -1,21 +1,22 @@
-# !/usr/bin/env python3
-# recursive functions, shows integer until insert 'cancel'
-def ask_for_int( prompt = "Please enter an integer:\n" ):
-    user_input = input( prompt ).strip()
-
-    if user_input.isdigit():
-        if int(user_input) % 2 == 0:
-            print(int(user_input) // 2)
-            ask_for_int()
-        else:
-            print(int(user_input) * 3 + 1)
-            ask_for_int()
-    elif user_input == 'cancel':
-        print('Bye!')
-        exit(0)
+#!/usr/bin/env python3
+def cod(n):
+    if n == "":
+        return ""
     else:
-        ask_for_int( prompt = "You need to pass an integer:\n" )
+        a = str(n[0]) + str(cod(n[1:]))
+        return int(a)
 
-
-some_number = ask_for_int()
-print(some_number)
+while 1:
+    inp = str(input("Введите строку (введите 'cancel' для выхода): "))
+    if inp == "cancel":
+        print("Bye!")
+        exit()
+    elif inp.isdigit():
+        num = cod(inp)
+        if num % 2 == 0:
+            num = num // 2
+        else:
+            num = num * 3 + 1
+        print(num)
+    else:
+        print("Не удалось преобразовать введенный текст в число.")
