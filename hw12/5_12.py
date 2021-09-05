34927295ff670ebc4ee0a48301598b993dcce98c
ELatestark <ravenjkeee@gmail.com>
2021-09-05 18:06:05 +0700

    edited prompt message

--- hw12/5_12.py
+++ hw12/5_12.py
@@ -2,7 +2,7 @@
 # collatz hypothesis. print number of steps in which variable num become == 1
 p2=0
 p1=1
-n=int(input('Insert disired n-th Fibonacci number : \n'))
+n=int(input('Insert desired n-th Fibonacci number : \n'))
 print(p2, p1, end=" ")
 for i in range(n-1):
     curr=p2+p1
